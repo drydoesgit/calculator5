14650ef9b1be8a3779187d1efe5a9e2279764715
drydoesgit <dy22@njit.edu>
2020-11-30 16:28:57 -0500

    Feature: Calculator

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,6 +1,6 @@
 import unittest
 import Calculator
-from CsvReader import CsvReader
+from CSVReader import CSVReader
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,28 +12,28 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.calculator, Calculator.Calculator)
 
     def test_addition_calculator(self):
-        test_data = CsvReader("src/Tests/Addition.csv").data
+        test_data = CSVReader("src/Tests/Addition.csv").data
         for row in test_data:
             result = int(row['Result'])
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_subtraction_calculator(self):
-        test_data = CsvReader("src/Tests/Subtraction.csv").data
+        test_data = CSVReader("src/Tests/Subtraction.csv").data
         for row in test_data:
             result = int(row['Result'])
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_multiplication_calculator(self):
-        test_data = CsvReader("src/Tests/Multiplication.csv").data
+        test_data = CSVReader("src/Tests/Multiplication.csv").data
         for row in test_data:
             result = int(row['Result'])
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_division_calculator(self):
-        test_data = CsvReader("src/Tests/Division.csv").data
+        test_data = CSVReader("src/Tests/Division.csv").data
         for row in test_data:
             result = round(float(row['Result']), 7)
             self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), result)
